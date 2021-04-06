c6b37248f43dcbcdfd9b6c102e3f5727145a1e51
yckj3552 <huangsusu@cloudwalk.com>
2021-04-06 16:35:09 +0800

    dp 环形抢劫

--- src/main/java/hss/basic/dynamicProcess/FeiBoNaQiRobbers.java
+++ src/main/java/hss/basic/dynamicProcess/FeiBoNaQiRobbers.java
@@ -32,6 +32,7 @@ import java.util.Scanner;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/house-robber
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * dp[i]=max(dp[i-2]+nums[i],dp[i-1])
  */
 
 public class FeiBoNaQiRobbers {
