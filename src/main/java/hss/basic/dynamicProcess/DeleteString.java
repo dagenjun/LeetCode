9c9552394a3f475bf2c7a9cb2d1caae3bdaf3eb9
yckj3552 <huangsusu@cloudwalk.com>
2021-04-16 18:03:33 +0800

    add String dp problem

---
+++ src/main/java/hss/basic/dynamicProcess/DeleteString.java
@@ -0,0 +1,44 @@
+package hss.basic.dynamicProcess;
+
+/**
+ * @author HSS
+ * @Date: 2021/4/16 11:08
+ * @Description: 583. 两个字符串的删除操作
+ * 给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 给定单词的长度不超过500。
+ * 给定单词中的字符只含有小写字母。
+ */
+public class DeleteString {
+    public static void main(String[] args) {
+        System.out.println(deleteString("sea","eat"));
+    }
+
+    private static int deleteString(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+
+            }
+        }
+        return n1+n2-dp[n1][n2]*2;
+    }
+}
